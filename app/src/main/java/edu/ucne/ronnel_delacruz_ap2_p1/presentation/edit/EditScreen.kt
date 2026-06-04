56c19aad9e03bfba6b2ab7acbaba8029e635e574
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-03 20:53:13 -0400

    Crea Pantalla de Edit con botones eliminar, guardar y el titulo

--- app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/edit/EditScreen.kt
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/edit/EditScreen.kt
@@ -1,2 +1,60 @@
 package edu.ucne.ronnel_delacruz_ap2_p1.presentation.edit
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditScreen(onNavigateBack: () -> Unit) {
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text("Crear") },
+                navigationIcon = {
+                    IconButton(onClick = onNavigateBack) {
+                        Icon(Icons.Default.ArrowBack, contentDescription = "Regresar")
+                    }
+                }
+            )
+        },
+        bottomBar = {
+            BottomAppBar {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    horizontalArrangement = Arrangement.SpaceEvenly
+                ) {
+                    OutlinedButton(onClick = onNavigateBack) {
+                        Text("Guardar")
+                    }
+                    OutlinedButton(
+                        onClick = onNavigateBack,
+                        colors = ButtonDefaults.outlinedButtonColors(
+                            contentColor = Color.Red
+                        )
+                    ) {
+                        Text("Eliminar")
+                    }
+                }
+            }
+        }
+    ) { padding ->
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(padding)
+        ) { }
+    }
+}
+@Preview(showBackground = true)
+@Composable
+fun EditScreenPreview() {
+    MaterialTheme {
+        EditScreen(onNavigateBack = {})
+    }
+}
