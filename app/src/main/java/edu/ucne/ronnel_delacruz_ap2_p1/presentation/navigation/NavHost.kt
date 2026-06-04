4ec1e4deebe073855c542dfa49ccf4d84d540ffd
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-03 20:55:04 -0400

    Crea el NavHost.kt con edit y list screen

--- app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/navigation/NavHost.kt
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/navigation/NavHost.kt
@@ -1,2 +1,27 @@
 package edu.ucne.ronnel_delacruz_ap2_p1.presentation.navigation
 
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import edu.ucne.ronnel_delacruz_ap2_p1.presentation.edit.EditScreen
+import edu.ucne.ronnel_delacruz_ap2_p1.presentation.list.ListScreen
+
+@Composable
+fun AppNavHost(navHostController: NavHostController) {
+    NavHost(
+        navController = navHostController,
+        startDestination = Screen.List
+    ) {
+        composable<Screen.List> {
+            ListScreen(
+                goToEdit = { navHostController.navigate(Screen.Edit) }
+            )
+        }
+        composable<Screen.Edit> {
+            EditScreen(
+                onNavigateBack = { navHostController.navigateUp() }
+            )
+        }
+    }
+}
