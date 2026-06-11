bc78deea27485129b687529cf3495fda2a1cb4d9
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-10 21:09:41 -0400

    Cambia los nombres del registro en el Screen.kt

--- app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/navigation/Screen.kt
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/presentation/navigation/Screen.kt
@@ -3,6 +3,6 @@ package edu.ucne.ronnel_delacruz_ap2_p1.presentation.navigation
 import kotlinx.serialization.Serializable
 
 sealed class Screen {
-    @Serializable data object List : Screen()
-    @Serializable data object Edit : Screen()
+    @Serializable data object AmonestacionList : Screen()
+    @Serializable data class Amonestacion(val amonestacionId: Int) : Screen()
 }
