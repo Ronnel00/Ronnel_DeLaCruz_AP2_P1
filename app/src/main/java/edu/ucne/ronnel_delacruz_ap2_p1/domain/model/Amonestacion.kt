8f8febd60886faa8ed70ebc75121321a9ed52265
Ronnel00 <ronnel_delacruz@ucne.edu.do>
2026-06-10 21:10:24 -0400

    Crea Amonestacion.kt

---
+++ app/src/main/java/edu/ucne/ronnel_delacruz_ap2_p1/domain/model/Amonestacion.kt
@@ -0,0 +1,8 @@
+package edu.ucne.ronnel_delacruz_ap2_p1.domain.model
+
+data class Amonestacion(
+    val amonestacionId: Int = 0,
+    val nombres: String = "",
+    val razon: String = "",
+    val monto: Double = 0.0
+)
